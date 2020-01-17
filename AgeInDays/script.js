9fdefcb92258fb2ecbe1060fe9bb5253d52eaa04
alexanderdimitrov333 <alexanderdimitrov333@gmail.com>
2020-01-17 22:21:15 +0200

    I made the button functions. The script was simple. In future i will make it more accurate. I will make it calculate the days from the exact time of birth to the pressing of the button.

---
+++ AgeInDays/script.js
@@ -0,0 +1,13 @@
+function question(){
+var birthYear = prompt("What year are you born in?");
+
+var ageInDays = (2020-birthYear)*365;
+var h1 = document.createElement('h1');
+var textAnswer = document.createTextNode('You are ' + ageInDays + ' days old!');
+h1.setAttribute('id','ageInDays');
+h1.appendChild(textAnswer);
+document.getElementById('flex-box-result').appendChild(h1);
+}
+function reset(){
+    document.getElementById('ageInDays').remove();
+}
